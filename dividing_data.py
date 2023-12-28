306b9539ac7e87c72811f8f1f21ccfae9e2c116d
dedesopian23 <sopiand772@gmail.com>
2023-12-28 22:06:36 +0700

    update

--- dividing_data.py
+++ dividing_data.py
@@ -79,7 +79,6 @@ if __name__ == "__main__":
                         print(f"Size of train Data (80%): {train_data.shape[0]} rows")
                         print(f"Size of test Data (20%): {test_data.shape[0]} rows")
 
-<<<<<<< HEAD
                         # Save the Bayesian and Kalman data to separate CSV files for each partition
                         train_data.to_csv(f'dataset/train_{file_name}_partition_{i}.csv', index=False)
                         test_data.to_csv(f'dataset/test_{file_name}_partition_{i}.csv', index=False)
@@ -87,18 +86,10 @@ if __name__ == "__main__":
                         test_data.to_csv(f'calibrated_dataset/test_{file_name}_partition_{i}.csv', index=False)
                     print("Train Data (First Partition):")
                     print(partitioned_data[0][0])
-=======
-                    # Save the Bayesian and Kalman data to separate CSV files for each partition
-                    train_data.to_csv(f'dataset_skema2/train_{file_name}_partition_{i}.csv', index=False)
-                    test_data.to_csv(f'dataset_skema2/test_{file_name}_partition_{i}.csv', index=False)
-                print("Train Data (First Partition):")
-                print(partitioned_data[0][0])
->>>>>>> a27aec3f1264f46e3de3af428e91797f3e4f92bf
 
                     print("Test Data (First Partition):")
                     print(partitioned_data[0][1])
 
-<<<<<<< HEAD
         #folder path dataset
         dataset_path = "dataset"
         dataset_calibration_path = "calibrated_dataset"
@@ -123,25 +114,6 @@ if __name__ == "__main__":
 
         if os.path.exists(dataset_path):
             dataset_list_path = os.listdir(dataset_path)
-=======
-    #folder path dataset
-    dataset_path = "dataset_skema2"
-
-    #folder path dataset test dan train
-    dataset_test_path = os.path.join(dataset_path, "test")
-    dataset_train_path = os.path.join(dataset_path, "train")
-
-    #folder path dataset test a23 dan f3
-    dataset_test_a23_path = os.path.join(dataset_test_path, "a23")
-    dataset_test_f3_path = os.path.join(dataset_test_path, "f3")
-
-    #folder path dataset train a23 dan f3
-    dataset_train_a23_path = os.path.join(dataset_train_path, "a23")
-    dataset_train_f3_path = os.path.join(dataset_train_path, "f3")
-    
-    if os.path.exists(dataset_path):
-        dataset_list_path = os.listdir(dataset_path)
->>>>>>> a27aec3f1264f46e3de3af428e91797f3e4f92bf
 
         #iterasi list file dataset
         for dataset_file in dataset_list_path:
@@ -343,7 +315,6 @@ if __name__ == "__main__":
     #     distances_maximum.append(euclidean_distance(test_column, maximum))
 
 
-<<<<<<< HEAD
         if os.path.exists(dataset_calibration_path):
             calibrated_dataset_list_path = os.listdir(dataset_calibration_path)
 
@@ -372,105 +343,3 @@ if __name__ == "__main__":
                 calibrated_dataset_file_origin = os.path.join(dataset_calibration_path, calibrated_dataset_file)
                 calibrated_dataset_file_destination = os.path.join(calibrated_train_f3_path, calibrated_dataset_file)
                 split_data(dataset_split_path=calibrated_train_f3_path, dataset_file=calibrated_dataset_file, dataset_file_origin=calibrated_dataset_file_origin)
-=======
-    # print(mean)
-    # print(median)
-    # print(maximum)
-    # print(test_dataset)
-    # print(f"Euclidean Distances for Partition {i} (WiFi_A, WiFi_B, WiFi_C, WiFi_D):")
-    # print("Mean Distance:", distances_mean)
-    # print("Median Distance:", distances_median)
-    # print("Maximum Distance:", distances_maximum)
-
-    
-#   # Calculate RMSE between test_data and first Mean, Median, and Maximum for each column
-#        # Calculate RMSE between test_data and first Mean, Median, and Maximum for each column
-    # rmse_mean = [rmse(test_dataset[column], np.full(len(test_dataset), mean)) for column in ["Wifi_A", "Wifi_B", "Wifi_C", "Wifi_D"]]
-    # rmse_median = [rmse(test_dataset[column], np.full(len(test_dataset), median)) for column in ["Wifi_A", "Wifi_B", "Wifi_C", "Wifi_D"]]
-    # rmse_maximum = [rmse(test_dataset[column], np.full(len(test_dataset), maximum)) for column in ["Wifi_A", "Wifi_B", "Wifi_C", "Wifi_D"]]
-
-
-
-    # print("RMSE:")
-    # print("Mean RMSE:", rmse_mean)
-    # print("Median RMSE:", rmse_median)
-    # print("Maximum RMSE:", rmse_maximum)
-    # standard_deviation = 1.0  # Adjust the standard deviation as needed
-    # probabilities_mean = [bayesian_likelihood_function(distance, standard_deviation) for distance in distances_mean]
-    # probabilities_median = [bayesian_likelihood_function(distance, standard_deviation) for distance in distances_median]
-    # probabilities_maximum = [bayesian_likelihood_function(distance, standard_deviation) for distance in distances_maximum]
-
-    # print("Probabilities (WiFi_A, WiFi_B, WiFi_C, WiFi_D):")
-    # print("Mean Probability:", probabilities_mean)
-    # print("Median Probability:", probabilities_median)
-    # print("Maximum Probability:", probabilities_maximum)
-
-
-
-
-
-    # for i, (train_data, test_data) in enumerate(partitioned_data, 1):
-    #     # Calculate Gaussian histograms for test data
-    #     test_data_hist = hist_gauss(test_data["Wifi_A"])  # You can change the column as needed
-    #     distances = []
-    #     for center in centers.values:
-    #         distance_val = euclidean_distance(test_data_hist, hist_gauss(center))
-    #         distances.append(distance_val)
-    #     print(f"Distance using Gaussian Histograms (Partition {i}):")
-    #     print(distances)
-    #     plot_histogram(test_data_hist, f'Gaussian Histogram for Test Data (Partition {i})')
-    #     print(f"Gaussian Histogram for Test Data (Partition {i}):")
-    #     print(test_data_hist)
-    
-    
-    # Load test data
-    # test_data = pd.read_csv("test_modified (0,1)_a23 - 08-08-2023 14-10-13.csv_partition_1.csv")
-    # train_data = pd.read_csv("train_modified (0,1)_a23 - 08-08-2023 14-10-13.csv_partition_1.csv")
-    # stat_data = pd.read_csv("Raw_statData_Wifi_A.csv")
-    # # print("Mean Colum RP = ['Mean','Median','Maximum']
-    # for target_column in AP:
-    #     # Extract the target column for train and test data
-    #     y_train = train_data[target_column]
-    #     y_test = test_data[target_column]
-
-    #     # Extract the selected features
-    #     X_train = train_data[target_column]
-    #     X_test = train_data[target_column]
-
-    #     # Create and train the Bayesian Ridge model
-    #     model = BayesianRidge()
-    #     model.fit(X_train, y_train)
-
-    #     # Perform Bayesian estimation on the test data
-    #     y_pred = model.predict(X_test)
-
-    #     # Calculate mean squared error as an example of performance evaluation
-    #     mse = mean_squared_error(y_test, y_pred)
-    #     print(f"Mean Squared Error for {target_column}: {mse}")
-
-    #     plt.figure(figsize=(8, 6))
-    #     plt.scatter(y_test, y_pred, alpha=0.5)
-    #     plt.xlabel("Actual Values")
-    #     plt.ylabel("Predicted Values")
-    #     plt.title(f"Bayesian Estimator for {target_column}")
-    #     plt.grid(True)
-    #     plt.show()n:")
-    # # print(train_data["Mean"])
-    # merged_test_data = test_data.join(stat_data.set_index(stat_data.index))
-    # merged_train_data = test_data.join(stat_data.set_index(stat_data.index))
-    # merged_test_data.to_csv("merged_test_data_a.csv", index=False)
-    # merged_train_data.to_csv("merged_train_data_a.csv", index=False)
-
-    # AP = ['Wifi_A', 'Wifi_B', 'Wifi_C', 'Wifi_D']
-    # #
-                # for i in range(10):
-                #     # shuffled_df.to_csv(f'dataset/shuffled_data_{file_name}.csv', index=False)
-                #     trainData, testData = partition_data(modified_df)
-                #     print(f"Size of Bayesian Data (80%): {trainData.shape[0]} rows")
-                #     trainData.to_csv(f'dataset/train_data_{file_name}.csv', index=False)
-                #     print(trainData)
-                #     print(f"Size of Kalman Data (20%): {testData.shape[0]} rows")
-                #     print(testData)
-                # trainData.to_csv("train.csv", index=False)
-                # testData.to_csv("test.csv", index=False)
->>>>>>> a27aec3f1264f46e3de3af428e91797f3e4f92bf
